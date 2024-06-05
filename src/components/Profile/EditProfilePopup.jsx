c3c622839e0c75df80964cd2285ea50d625daaab
Prvzalm <prvzalm69@gmail.com>
2024-06-05 17:18:32 +0530

    Subscription RA popup fixed

--- src/components/Profile/EditProfilePopup.jsx
+++ src/components/Profile/EditProfilePopup.jsx
@@ -164,11 +164,11 @@ const EditProfilePopup = ({ isOpen, onClose, onUpdateProfile, userData }) => {
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                   placeholder=" "
-                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent rounded-lg border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-[#ffffff4b] focus:outline-none focus:ring-0 focus:border-white peer"
+                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-white peer"
                 />
                 <label
                   htmlFor="name"
-                  className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-[#18181B] px-2 peer-focus:px-2 peer-focus:text-white-600 peer-focus:dark:text-white peer-focus:border-2 peer-focus:border-[#ffffff47] peer-focus:rounded-md peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                  className="absolute text-sm text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-transparent px-2 peer-focus:px-2 peer-focus:text-white peer-focus:border-2 peer-focus:border-[#ffffff47] peer-focus:rounded-md peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4"
                 >
                   Name
                 </label>
@@ -184,11 +184,11 @@ const EditProfilePopup = ({ isOpen, onClose, onUpdateProfile, userData }) => {
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     placeholder=" "
-                    className="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent rounded-lg border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-[#ffffff4b] focus:outline-none focus:ring-0 focus:border-white peer"
+                    className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-white peer"
                   />
                   <label
                     htmlFor="email"
-                    className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-[#18181B] px-2 peer-focus:px-2 peer-focus:text-white-600 peer-focus:dark:text-white peer-focus:border-2 peer-focus:border-[#ffffff47] peer-focus:rounded-md peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                    className="absolute text-sm text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-transparent px-2 peer-focus:px-2 peer-focus:text-white peer-focus:border-2 peer-focus:border-[#ffffff47] peer-focus:rounded-md peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4"
                   >
                     Email
                   </label>
@@ -202,11 +202,11 @@ const EditProfilePopup = ({ isOpen, onClose, onUpdateProfile, userData }) => {
                     value={phone}
                     onChange={(e) => setPhone(e.target.value)}
                     placeholder=" "
-                    className="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent rounded-lg border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-[#ffffff4b] focus:outline-none focus:ring-0 focus:border-white peer"
+                    className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-white peer"
                   />
                   <label
                     htmlFor="phone"
-                    className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-[#18181B] px-2 peer-focus:px-2 peer-focus:text-white-600 peer-focus:dark:text-white peer-focus:border-2 peer-focus:border-[#ffffff47] peer-focus:rounded-md peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                    className="absolute text-sm text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-transparent px-2 peer-focus:px-2 peer-focus:text-white peer-focus:border-2 peer-focus:border-[#ffffff47] peer-focus:rounded-md peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4"
                   >
                     Phone Number
                   </label>
@@ -222,11 +222,11 @@ const EditProfilePopup = ({ isOpen, onClose, onUpdateProfile, userData }) => {
                   value={panCard}
                   onChange={(e) => setPanCard(e.target.value)}
                   placeholder=" "
-                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent rounded-lg border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-[#ffffff4b] focus:outline-none focus:ring-0 focus:border-white peer"
+                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-white peer"
                 />
                 <label
                   htmlFor="panCard"
-                  className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-[#18181B] px-2 peer-focus:px-2 peer-focus:text-white-600 peer-focus:dark:text-white peer-focus:border-2 peer-focus:border-[#ffffff47] peer-focus:rounded-md peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                  className="absolute text-sm text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-transparent px-2 peer-focus:px-2 peer-focus:text-white peer-focus:border-2 peer-focus:border-[#ffffff47] peer-focus:rounded-md peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4"
                 >
                   PAN Card
                 </label>
@@ -238,11 +238,11 @@ const EditProfilePopup = ({ isOpen, onClose, onUpdateProfile, userData }) => {
                   value={state}
                   onChange={(e) => setState(e.target.value)}
                   placeholder=" "
-                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent rounded-lg border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-[#ffffff4b] focus:outline-none focus:ring-0 focus:border-white peer"
+                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-white peer"
                 />
                 <label
                   htmlFor="state"
-                  className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-[#18181B] px-2 peer-focus:px-2 peer-focus:text-white-600 peer-focus:dark:text-white peer-focus:border-2 peer-focus:border-[#ffffff47] peer-focus:rounded-md peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                  className="absolute text-sm text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-transparent px-2 peer-focus:px-2 peer-focus:text-white peer-focus:border-2 peer-focus:border-[#ffffff47] peer-focus:rounded-md peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4"
                 >
                   State
                 </label>
@@ -257,11 +257,11 @@ const EditProfilePopup = ({ isOpen, onClose, onUpdateProfile, userData }) => {
                   value={address}
                   onChange={(e) => setAddress(e.target.value)}
                   placeholder=" "
-                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent rounded-lg border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-[#ffffff4b] focus:outline-none focus:ring-0 focus:border-white peer"
+                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-white peer"
                 />
                 <label
                   htmlFor="address"
-                  className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-[#18181B] px-2 peer-focus:px-2 peer-focus:text-white-600 peer-focus:dark:text-white peer-focus:border-2 peer-focus:border-[#ffffff47] peer-focus:rounded-md peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                  className="absolute text-sm text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-transparent px-2 peer-focus:px-2 peer-focus:text-white peer-focus:border-2 peer-focus:border-[#ffffff47] peer-focus:rounded-md peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4"
                 >
                   Address
                 </label>
--- src/components/Subscription RA/MobileCourse.js
+++ src/components/Subscription RA/MobileCourse.js
@@ -3,56 +3,40 @@ import React from "react";
 function MobileCourse({ showMobilePopup, handleBuyNowClick, subscriptions }) {
   return (
     <div>
-      {showMobilePopup && (
+      {showMobilePopup && subscriptions.length > 1 && (
         <div className="fixed bottom-0 left-0 right-0 bg-black p-4 shadow-lg z-50">
-          {subscriptions.length >= 1 && (
-            <div className="flex rounded-2xl items-center bg-[#18181B80]">
-              <div className="flex-1 text-white text-left">
-                <div className="flex flex-col">
+          <div className="flex rounded-2xl items-center bg-[#18181B80]">
+            <div className="flex-1 text-white text-left">
+              <div className="flex flex-col">
+                <span className="text-lg text-white">
+                  {subscriptions[1].planType}
+                </span>
+                <div className="flex items-center gap-3">
                   <span className="text-lg text-white">
-                    {subscriptions.length === 1
-                      ? subscriptions[0].planType
-                      : subscriptions[1].planType}
+                    <del>₹5,999</del>
                   </span>
-                  <div className="flex items-center gap-3">
-                    <span className="text-lg text-white">
-                      <del>₹5,999</del>
-                    </span>
-                    <span className="text-2xl font-bold text-white">
-                      ₹
-                      {subscriptions.length === 1
-                        ? subscriptions[0].amount
-                        : subscriptions[1].amount}
-                    </span>
-                  </div>
+                  <span className="text-2xl font-bold text-white">
+                    ₹{subscriptions[1].amount}
+                  </span>
                 </div>
-                <p className="text-[#C6CDD5] text-sm">
-                  {subscriptions.length === 1
-                    ? subscriptions[0].durationMonth
-                    : subscriptions[1].durationMonth}{" "}
-                  Month Access
-                </p>
               </div>
-              <button
-                onClick={() =>
-                  handleBuyNowClick(
-                    subscriptions.length === 1
-                      ? subscriptions[0].id
-                      : subscriptions[1].id,
-                    subscriptions.length === 1
-                      ? subscriptions[0].planType
-                      : subscriptions[1].planType,
-                    subscriptions.length === 1
-                      ? subscriptions[0].amount
-                      : subscriptions[1].amount
-                  )
-                }
-                className="text-lg px-5 rounded-lg font-semibold flex items-center py-3 bg-[#fff] text-[#000] hover:bg-[#000] hover:text-[#fff]"
-              >
-                Buy Now
-              </button>
+              <p className="text-[#C6CDD5] text-sm">
+                {subscriptions[1].durationMonth} Month Access
+              </p>
             </div>
-          )}
+            <button
+              onClick={() =>
+                handleBuyNowClick(
+                  subscriptions[1].id,
+                  subscriptions[1].planType,
+                  subscriptions[1].amount
+                )
+              }
+              className="text-lg px-5 rounded-lg font-semibold flex items-center py-3 bg-[#fff] text-[#000] hover:bg-[#000] hover:text-[#fff]"
+            >
+              Buy Now
+            </button>
+          </div>
         </div>
       )}
     </div>
